e136bbd9820770e75c81adc35ca40894bb62021f
Aleex1237 <alexis10893@outlook.com>
2021-07-17 23:29:03 -0300

    agregando titles, aboutMeEjs

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3000
-app.use(express.static("public"))
+
 
 //set view engine
 app.set("view engine", "ejs")
 
 //Asignando carpeta views
 app.set("views", path.join(__dirname, "views"))
+
+app.use(express.static(path.join(__dirname, "public")));
 
 //REQUIRIENDO ROUTES
 const indexRouter = require("./routes/indexRouter")
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,22 +1,22 @@
 module.exports = {
     index: (req, res) => {
-        return res.render("home")
+        return res.render("home",{title: "Inicio"})
     },
 
     contact: (req, res) => {
-        return res.render("contact")
+        return res.render("contact",{title: "Contacto"})
     },
 
 
     services: (req, res) => {
-        return res.render("services")
+        return res.render("services",{title: "Servicios"})
     },
 
     aboutMe: (req, res) => {
-        return res.render("aboutMe")
+        return res.render("aboutMe",{title: "Sobre mi"})
     },
 
     meme: (req, res) => {
-        return res.render("meme")
+        return res.render("meme",{title: "Meme"})
     }
 }
